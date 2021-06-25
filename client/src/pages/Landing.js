bb79727714484623f0bc2132a2a113c0741a9973
shangxian Piao <sj212131@hotmail.com>
2021-06-24 21:25:26 -0400

    upload pages.js

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Landing extends Component {
+  render() {
+    return (
+      <div>
+        <p>This is Landing part</p>
+      </div>
+    );
+  }
+}
+
+export default Landing;
