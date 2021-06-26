0c89a57948d70b74290a2a67f10298b0ab13adf8
Taylor Leong <tjl2125@alum.barnard.edu>
2021-06-26 05:18:01 -0400

    added to components and added to landing.js

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -4,8 +4,36 @@ class Landing extends Component {
   render() {
     return (
       <div>
+      <div>
         <p>This is Landing part</p>
       </div>
+
+      <div className="field">
+      <p className="control">
+        <span className="select">
+          <select>
+            <option>Select User Type</option>
+            <option>Resident</option>
+            <option>Management</option>
+          </select>
+        </span>
+      </p>
+    </div>
+    
+      <div className="field">
+      <div className="control">
+        <input className="input" type="text" placeholder="Username" />
+        <input className="input" type="text" placeholder="Password" />
+      </div>
+    </div>
+    
+    
+    
+    <div className="buttons">
+      <a className="button is-link">Sign Up</a>
+      <a className="button is-link">Sign In</a>
+    </div>
+    </div>
     );
   }
 }
