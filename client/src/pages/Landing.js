8801be173114de64f05dbed0e3c19ea530ae55bf
Taylor Leong <76642193+tjl2125@users.noreply.github.com>
2021-06-26 11:42:32 -0400

    added dropdown menu and buttons (#19)

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -7,20 +7,63 @@ class Landing extends Component {
       <div>
         <p>This is Landing part</p>
       
-
-        <div className="field">
-            <label className="label" htmlFor="email">Email</label>
-            <div className="control">
-              <input className="input" name="email" type="email" placeholder="email" required onChange={this.handleChange} />
-            </div>
+        <div className = "field is-grouped is-grouped-centered">
+        <div class="control">
+          <div class="select">
+          <select>
+            <option>Select User Type</option>
+            <option>Resident</option>
+            <option>Property Manager</option>
+          </select>
           </div>
+       </div>
+       </div>
 
-          <div className="field">
-            <label className="label" htmlFor="password">Password</label>
+        <div className="field is-grouped is-grouped-centered">
+
+            <label className="label" 
+            htmlFor="username">
+              Username
+              </label>
+
             <div className="control">
-              <input className="input" name="password" type="password" placeholder="password" required onChange={this.handleChange}/>
+              <input className="input" 
+              style={{ width: '400px' }} 
+              type="text" 
+              placeholder="Username" />
             </div>
-          </div>
+        </div>
+
+        <div className="field is-grouped is-grouped-centered">
+
+            <label className="label" 
+            htmlFor="password">
+              Password
+              </label>
+
+            <div className="control">
+              <input className="input" 
+              style={{ width: '400px' }} 
+              name="password" 
+              type="password" 
+              placeholder="password" />
+            </div>
+        </div>
+
+        <div class="field is-grouped is-grouped-centered">
+            <p class="control">
+              <button class="button is-primary" onClick={() => console.log(form)}>
+              Sign Up 
+              </button>
+           </p>
+            <p class="control">
+              <button class="button is-success">
+                Sign In
+              </button>
+            </p>
+  
+        </div>
+
       </div>
 
     );
