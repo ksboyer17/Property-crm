90291dba1cb0da56effa65c9a1855d2dd106f98c
Taylor Leong <tjl2125@alum.barnard.edu>
2021-06-26 23:43:31 -0400

    adding to PMCreate to make the boxes

--- client/src/pages/PMCreate.js
+++ client/src/pages/PMCreate.js
@@ -5,6 +5,29 @@ class PMCreate extends Component {
     return (
       <div>
         <p>This is PMCreate part</p>
+        <div className = "columns">
+          <div className = "column">
+
+            <form className="box user-info">
+              <div class="field">
+                <label class="label">First and Last Name</label>
+                <div class="control">
+                  <input class="input" type="text" placeholder="First and Last Name" />
+                </div>
+              </div>
+
+              <div class="field">
+                <label class="label">Password</label>
+                <div class="control">
+                  <input class="input" type="password" placeholder="********" />
+                </div>
+              </div>
+
+              <button class="button is-primary">Sign in</button>
+            </form>
+          </div>
+        </div>
+
       </div>
     );
   }
